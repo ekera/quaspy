8488fbbe859b685926ba9726f20d309b30a903ec
Martin Ekerå <47600538+ekera@users.noreply.github.com>
2023-10-28 19:59:17 +0200

    Added a missing reference

--- src/quaspy/factoring/general/__init__.py
+++ src/quaspy/factoring/general/__init__.py
@@ -3,6 +3,10 @@
     This module uses Shor's order-finding algorithm to factor general integers,
     as described in [Shor94], with improvements from [E21b] and [E22p].
 
+    [Shor94] Shor, P.W.: "Algorithms for Quantum Computation: Discrete
+                          Logarithms and Factoring".
+                         In: Proceedings from FOCS '94, pp. 124–134 (1994).
+
     [E21b] Ekerå, M.: "On completely factoring any integer efficiently in a
                        single run of an order-finding algorithm".
                        Quantum Inf. Process. 20(6):205 (2021).
